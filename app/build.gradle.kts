3ca1477199a0f6106306f201b73ec1a1dd9700b9
Xires87 <lukasz005719@gmail.com>
2026-03-02 11:26:19 +0100

    usunieto niepotrzebna biblioteke

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
